0fb4ce5c6bfa846663843a88378cca0816a1e258
Therese Hagwall <90thejag@skola.boras.se>
2023-01-19 11:23:00 +0100

    fix bugs

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,34 @@
 const body = document.querySelector("body") as HTMLBodyElement;
 const container = document.createElement("div");
 const nxtButton = document.createElement("button");
-// const prevButton = document.createElement("button");
 body.append(container);
 container.append(nxtButton);
-
+let joke = document.createElement("p");
+let delivery = document.createElement("article");
 nxtButton.innerHTML = "Next";
-// prevButton.innerHTML = "Prev";
 
 
 
 async function jokesData() {
     const response = await fetch("https://v2.jokeapi.dev/joke/Any");
     const data = await response.json();
-    
-    const printInfo = function(){
-    let joke = document.createElement("p");
-    let delivery = document.createElement("article");
-    
-    joke.innerHTML = data.joke;
-    container.append(joke);
-    if(joke.innerHTML === "undefined"){
-        container.append(delivery);
-        joke.innerHTML = data.setup;
-        delivery.innerHTML = data.delivery;
-    }
-}
-
-nxtButton.addEventListener("click", function(e){
-    // e.preventDefault();
-    let joke = document.querySelector("p") as HTMLParagraphElement;
-    let delivery = document.querySelector("article") as HTMLElement;
     joke.innerHTML = "";
     delivery.innerHTML = "";
-    console.log(data.setup, data.joke);
-    jokesData();
-})
+    const printInfo = function(){
+        joke.innerHTML = data.joke;
+        container.append(joke);
+        if(joke.innerHTML === "undefined"){
+            container.append(delivery);
+            joke.innerHTML = data.setup;
+            delivery.innerHTML = data.delivery;
+        }
+}
 printInfo();
-    
+
 };
 
 jokesData();
 
+nxtButton.addEventListener("click", function(){
+    jokesData();
+})
